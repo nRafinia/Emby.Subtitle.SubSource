fbf4b3237aaa63c3ea8ae21d92d554578ee19659
Naser <naser.r@gmail.com>
2025-07-27 19:12:24 +0330

    Adding

--- src/Emby.Subtitle.Subsource/Models/DownloadResponse.cs
+++ src/Emby.Subtitle.Subsource/Models/DownloadResponse.cs
@@ -2,28 +2,11 @@
 {
     public class DownloadResponse
     {
-        public Subtitle subtitle { get; set; }
+        public SubtitleDownload subtitle { get; set; }
+    }
 
-        public class Subtitle
-        {
-            public int id { get; set; }
-            public string uploaded_at { get; set; }
-            public string language { get; set; }
-            public string rating { get; set; }
-            public int uploaded_by { get; set; }
-            public string[] release_info { get; set; }
-            public string commentary { get; set; }
-            public object files { get; set; }
-            public object size { get; set; }
-            public int downloads { get; set; }
-            public object production_type { get; set; }
-            public object release_type { get; set; }
-            public object hearing_impaired { get; set; }
-            public object foreign_parts { get; set; }
-            public object framerate { get; set; }
-            public object preview { get; set; }
-            public bool user_uploaded { get; set; }
-            public string download_token { get; set; }
-        }
+    public class SubtitleDownload
+    {
+        public string download_token { get; set; }
     }
 }
